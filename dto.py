93b7a8382f06f4543c21b925f601f06782b94aee
patelinadev <liana255c@outlook.com>
2025-09-16 00:44:22 -0400

    feat(triangles): implement triangle related drawing feature with tests

--- dto.py
+++ dto.py
@@ -12,3 +12,66 @@ class HollowSquareRequestDto(BaseModel):
 
 class HollowSquareResponseDto(BaseModel):
     result: str
+
+class EquilateralTriangleRequestDto(BaseModel):
+    base_length:int
+
+class EquilateralTriangleResponseDto(BaseModel):
+    result: str
+
+class IsoscelesTriangle01RequestDto(BaseModel):
+    base_length: int
+
+class IsoscelesTriangle01ResponseDto(BaseModel):
+    result: str
+
+class IsoscelesTriangle02RequestDto(BaseModel):
+    base_length: int
+    height: int
+
+class IsoscelesTriangle02ResponseDto(BaseModel):
+    result: str
+
+class HollowIsoscelesTriangle01RequestDto(BaseModel):
+    base_length:int
+
+class HollowIsoscelesTriangle01ResponseDto(BaseModel):
+    result: str
+
+class HollowIsoscelesTriangle02RequestDto(BaseModel):
+    base_length: int
+    height: int
+
+class HollowIsoscelesTriangle02ResponseDto(BaseModel):
+    result: str
+# ---Trapezoid---
+class TrapezoidRequestDto(BaseModel):
+    base_length: int
+    height: int
+
+class TrapezoidResponseDto(BaseModel):
+    result: str
+# ---Diamond---
+class DiamondRequestDto(BaseModel):
+    side_length:int
+
+class DiamondResponseDto(BaseModel):
+    result: str
+
+class HollowDiamondRequestDto(BaseModel):
+    side_length:int
+
+class HollowDiamondResponseDto(BaseModel):
+    result:str
+
+class HollowRectangleDiamondRequestDto(BaseModel):
+    side_length:int
+
+class HollowRectangleDiamondResponseDto(BaseModel):
+    result: str
+# ---Hexagon---
+class HexagonRequestDto(BaseModel):
+    side_length:int
+
+class HexagonResponseDto(BaseModel):
+    result: str
