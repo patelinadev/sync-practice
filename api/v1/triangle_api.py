edbebccf10d3a5b8d3a516d116e8d056ed40d4c8
patelinadev <liana255c@outlook.com>
2025-09-16 23:55:23 -0400

    refactor(DI): implement dependency injection for service

--- api/v1/triangle_api.py
+++ api/v1/triangle_api.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from dto import *
-from service.geometry_service import geometry_service
+from service.geometry_service import GeometryService, get_geometry_service
 
 router = APIRouter(
     prefix="/triangle",
@@ -8,26 +8,26 @@ router = APIRouter(
 )
 
 @router.post("/draw_equilateral_triangle",response_model = EquilateralTriangleResponseDto)
-async def draw_equilateral_triangle(request_dto: EquilateralTriangleRequestDto):
-    ascii_shape = geometry_service.draw_equilateral_triangle(request_dto)
+async def draw_equilateral_triangle(request_dto: EquilateralTriangleRequestDto, service: GeometryService = Depends(get_geometry_service)):
+    ascii_shape = service.draw_equilateral_triangle(request_dto)
     return EquilateralTriangleResponseDto(result = ascii_shape)
 
 @router.post("/draw_isosceles_triangle01", response_model = IsoscelesTriangle01ResponseDto)
-async def draw_equilateral_triangle01(request_dto: IsoscelesTriangle01RequestDto):
-    ascii_shape = geometry_service.draw_isosceles_triangle01(request_dto)
+async def draw_equilateral_triangle01(request_dto: IsoscelesTriangle01RequestDto, service: GeometryService = Depends(get_geometry_service)):
+    ascii_shape = service.draw_isosceles_triangle01(request_dto)
     return IsoscelesTriangle01ResponseDto(result = ascii_shape)
 
 @router.post("/draw_isosceles_triangle02", response_model = IsoscelesTriangle02ResponseDto)
-async def draw_isosceles_triangle02(request_dto: IsoscelesTriangle02RequestDto):
-    ascii_shape = geometry_service.draw_isosceles_triangle02(request_dto)
+async def draw_isosceles_triangle02(request_dto: IsoscelesTriangle02RequestDto, service: GeometryService = Depends(get_geometry_service)):
+    ascii_shape = service.draw_isosceles_triangle02(request_dto)
     return IsoscelesTriangle02ResponseDto(result = ascii_shape)
 
 @router.post("/draw_hollow_isosceles_triangle01", response_model = HollowIsoscelesTriangle01ResponseDto)
-async def draw_hollow_isosceles_triangle01(request_dto: HollowIsoscelesTriangle01RequestDto):
-    ascii_shape = geometry_service.draw_hollow_isosceles_triangle01(request_dto)
+async def draw_hollow_isosceles_triangle01(request_dto: HollowIsoscelesTriangle01RequestDto, service: GeometryService = Depends(get_geometry_service)):
+    ascii_shape = service.draw_hollow_isosceles_triangle01(request_dto)
     return HollowIsoscelesTriangle01ResponseDto(result = ascii_shape)
 
 @router.post("/draw_hollow_isosceles_triangle02", response_model = HollowIsoscelesTriangle02ResponseDto)
-async def draw_hollow_isosceles_triangle02(request_dto: HollowIsoscelesTriangle02RequestDto):
-    ascii_shape = geometry_service.draw_hollow_isosceles_triangle02(request_dto)
+async def draw_hollow_isosceles_triangle02(request_dto: HollowIsoscelesTriangle02RequestDto, service: GeometryService = Depends(get_geometry_service)):
+    ascii_shape = service.draw_hollow_isosceles_triangle02(request_dto)
     return HollowIsoscelesTriangle02ResponseDto(result = ascii_shape)
